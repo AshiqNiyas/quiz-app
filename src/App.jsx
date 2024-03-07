be431afdc5fb2332df3a01227f3c9fa2a6e6c354
Ashiq N <ashiqniyas.dev@gmail.com>
2024-03-07 19:29:01 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,13 @@ import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Navbar from "./components/Navbar";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-function App() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/");
-  }, []);
 
+function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/result" element={<Result />} />
       </Routes>
